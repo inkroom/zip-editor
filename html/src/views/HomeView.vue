58e93890737d685faaf5930936dc986038835e8c
inkbox <panxiaoyu@xiyoumail.com>
2024-02-06 16:55:43 +0800

    添加右侧翻页按钮

--- html/src/views/HomeView.vue
+++ html/src/views/HomeView.vue
@@ -455,6 +455,31 @@ export default {
               </svg> </template>
           </n-button>
         </div>
+        <div class="right-button" v-if="hide">
+          <n-button @click="prev" quaternary>
+            <!-- 上一页 -->
+            <template #icon>
+              <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 20 20">
+                <g fill="none">
+                  <path
+                    d="M6 4.75a.75.75 0 0 0-.743.648L5.25 5.5v9a.75.75 0 0 0 1.493.102l.007-.102v-9A.75.75 0 0 0 6 4.75zm8.28.22a.75.75 0 0 0-.976-.073l-.084.073l-4.5 4.5a.75.75 0 0 0-.073.976l.073.084l4.5 4.5a.75.75 0 0 0 1.133-.976l-.073-.084L10.31 10l3.97-3.97a.75.75 0 0 0 0-1.06z"
+                    fill="currentColor"></path>
+                </g>
+              </svg>
+            </template>
+          </n-button>
+          <n-button @click="next" quaternary>
+            <!-- 下一页 -->
+            <template #icon>
+              <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 20 20">
+                <g fill="none">
+                  <path
+                    d="M13.75 4.75a.75.75 0 0 1 .743.648l.007.102v9a.75.75 0 0 1-1.493.102L13 14.5v-9a.75.75 0 0 1 .75-.75zm-8.28.22a.75.75 0 0 1 .976-.073l.084.073l4.5 4.5a.75.75 0 0 1 .073.976l-.073.084l-4.5 4.5a.75.75 0 0 1-1.133-.976l.073-.084L9.44 10L5.47 6.03a.75.75 0 0 1 0-1.06z"
+                    fill="currentColor"></path>
+                </g>
+              </svg> </template>
+          </n-button>
+        </div>
     <div class="left tree" v-if="!hide">
 
       <n-tree :data="data" v-if="hasFile" @on-select-change="handleTreeClick" :expand-on-click="true"
@@ -658,6 +683,18 @@ export default {
       display: block;
       margin-left:0px !important;
       padding-left: 0px !important;
+      width: 40px;
+      height: 40px;
+    }
+  }
+  .right-button{
+    position: fixed;
+    top: 50%;
+    right: 8px;
+    button{
+      display: block;
+      margin-right:0px !important;
+      padding-right: 0px !important;
       width: 40px;
       height: 40px;
     }
